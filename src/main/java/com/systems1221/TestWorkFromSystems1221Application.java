243c8bb02bc18fb59272ebf5b1cb8d5235324392
IKS-cod <ik.smirnov@yandex.ru>
2025-03-19 19:12:53 +0300

    Initial commit

---
+++ src/main/java/com/systems1221/TestWorkFromSystems1221Application.java
@@ -0,0 +1,13 @@
+package com.systems1221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWorkFromSystems1221Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWorkFromSystems1221Application.class, args);
+	}
+
+}
